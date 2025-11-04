87449cc4066f0b7bafacf24c9693f70abd3072bb
saramohamed504 <92696069+sara55m@users.noreply.github.com>
2025-11-04 18:39:04 +0200

    booking task

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\DoctorController;
+use App\Http\Controllers\Api\PatientController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('bookings', [BookingController::class, 'create']);
+Route::get('bookings/{booking}', [BookingController::class, 'show']);
+Route::put('bookings/{booking}/update', [BookingController::class, 'update']);
+Route::delete('bookings/{booking}/cancel', [BookingController::class, 'destroy']);
+
+Route::get('bookings/doctor', [BookingController::class, 'doctorBookings']);
+Route::get('bookings/patient', [BookingController::class, 'patientBookings']);
